1a58616ae171a0e4c54695c849786ed0cd7f1d13
Tom Mount <tmountjr@gmail.com>
2023-10-10 17:43:29 -0400

    after consultation with raees

--- edge-functions/graphql.js
+++ edge-functions/graphql.js
@@ -1,28 +1,32 @@
+import { URL } from 'whatwg-url'
+
 /**
  * 
  * @param {Request} request The original request
  * @returns Response
  */
 export const handleHttpRequest = async (request) => {
-  const body = await request.text()
-  const resp = await fetch('https://graphqlzero.almansi.me/api', {
-    method: 'POST',
-    body,
-    headers: {
-      'content-type': 'application/json',
-      accept: 'application/json'
-    },
+  const currentUrl = new URL(request.url)
+  const newUrl = new URL(currentUrl.pathname, ['localhost', '127.0.0.1'].includes(currentUrl.host) ? 'https://se-apps-ef-graphql-response-default.edgio.link' : currentUrl.origin);
+
+  const newRequest = new Request(newUrl.toString(), {
+    method: request.method,
+    headers: request.headers,
+    body: await request.arrayBuffer()
+  })
+
+  const response = await fetch(newRequest, {
     edgio: {
       origin: 'origin'
     }
   })
 
-  const respBody = await resp.text()
-  return new Response(respBody, {
-    headers: {
-      'x-graphql-keys': 'graphql:Query',
-      'content-type': 'application/json'
-    },
-    status: 290
-  })
+  const isCacheable = response.headers.get('x-cacheable')
+  if (isCacheable === 'true') {
+    response.headers.set('cache-control', 's-maxage=600')
+  } else {
+    response.headers.set('cache-control', 'no-cache')
+  }
+
+  return response
 }
--- routes.js
+++ routes.js
@@ -1,25 +1,11 @@
-import { Router, edgioRoutes, and } from '@edgio/core'
+import { Router, edgioRoutes } from '@edgio/core'
 
 export default new Router()
   .use(edgioRoutes)
-  .if(
-    and(
-      { method: "POST" },
-      { path: "/api" },
-      { edgeControlCriteria: { "===": [{ response: "status_code" }, "290"] } }
-    ),
-    {
-      caching: {
-        enable_caching_for_methods: ["POST"]
-      },
-      headers: {
-        add_response_headers: { "x-foo": "bar" }
-      },
-      response: {
-        set_status_code: 200
-      }
-    }
-  )
   .post('/api', {
+    caching: {
+      enable_caching_for_methods: ["POST"]
+    },
     edge_function: './edge-functions/graphql.js'
   })
+  
